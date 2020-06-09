01d6d26ca71ad0f60c5cc2e372c65e62f0f6cd95
Stephan Richter <github@keawe.de>
2020-06-10 00:19:19 +0200

    added Translations

--- src/main/java/de/keawe/examples/localconfig/App.java
+++ src/main/java/de/keawe/examples/localconfig/App.java
@@ -2,20 +2,30 @@ package de.keawe.examples.localconfig;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.keawe.localconfig.Configuration;
+import de.keawe.tools.translations.Translation;
 
 public class App {
+	private static Logger log = LoggerFactory.getLogger(App.class);
+	
+	private static String t(String txt,Object...fills) {
+		return Translation.get(App.class, txt, fills);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		String filename = Configuration.dir("ConfigTest.config");
-		System.out.println("Storing configuration in "+filename);
+		log.debug(t("Storing configuration in {}."),filename);
 		
 		Configuration config = new Configuration(filename);
 		config.put("my_long", 1234);
-		config.put("my_String", "Dies ist ein Test");
-		System.out.println(config);
+		config.put("my_String", t("This is a test"));
+		log.debug("{}",config);
 		config.save();
 		
 		Configuration cf2 = new Configuration(filename);
-		System.out.println(cf2);
+		log.debug("{}",cf2);
 	}
 }
